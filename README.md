992559a290b61cc2b1e24937de3a8417cc9bf043
Rafael Mumme <rmumme@hugeinc.com>
2013-02-01 17:14:20 -0500

    readme update and changes to the api

--- README.md
+++ README.md
@@ -6,26 +6,92 @@
 npm install smart-ranking
 ```
 
-## How to use it
+## Usage
 
 ```js
-var Ranking = require('smart-ranking');
-
 var ratings = [
-    { votes: [3,2,1], id:'Item 1' },
-    { votes: [1,2,3,4,5], id:'Item 2' }
+    { votes: [3,2,1], id:'Batman' },
+    { votes: [1,2,3,4,5], id:'Robin' }
 ];
 
+var Ranking = require('smart-ranking');
+
 var bayesianScore = Ranking.bayesian(ratings);
-var amazonScore = Ranking.amazon(ratings);
+var imdbScore = Ranking.imdb(ratings);
 ```
 
-## Output example
+### Input
+
+Three different inputs are accepted:
 
 ```js
-	amazonScore = {
-		"Item 1":{"_id":"Item 1","_sumVotes":6,"_numVotes":3,"_avgRating":2,"score":2.3125},
-		"Item 2":{"_id":"Item 2","_sumVotes":15,"_numVotes":5,"_avgRating":3,"score":2.859375},
-		"_avgRatings":2.625,"_avgVotes":4
+var ratings = [
+    { votes: [3,2,1], id:'Batman' },
+    { votes: [1,2,3,4,5], id:'Robin' }
+];
+```
+
+```js
+var ratings = [
+	{ sumVotes: 10, numVotes:3, _id:{ id:'Batman' } },
+	{ sumVotes: 10, numVotes:3, _id:{ id:'Robin' } }
+];
+```
+
+```js
+var ratings = [
+	{ sumVotes: 10, numVotes:3, id:'Batman' },
+	{ sumVotes: 10, numVotes:3, id:'Robin' }
+];
+```
+
+## Output
+
+Two different outputs are available:
+
+```js
+	// returns an object
+	var imdbScore = Ranking.imdb(ratings);
+	imdbScore = {
+		"Item 1":{"id":"Item 1","sumVotes":6,"numVotes":3,"avgRating":2,"score":2.3125},
+		"Item 2":{"id":"Item 2","sumVotes":15,"numVotes":5,"avgRating":3,"score":2.859375},
+		"avgRatings":2.625,
+		"avgVotes":4
 	}
 ```
+
+```js
+	// returns an array
+	var imdbScore = Ranking.imdb(ratings, true);
+	imdbScore = [
+		{"id":"Item 1","sumVotes":6,"numVotes":3,"avgRating":2,"score":2.3125},
+		{"id":"Item 2","sumVotes":15,"numVotes":5,"avgRating":3,"score":2.859375}
+	]
+```
+
+## Api
+
+### Ranking.imdb(ratings, returnArray, min);
+
+Parameters:
+. ratings (Array)
+	Array with data 
+	* see input options above
+. returnArray (boolean)
+	If true, the method returns an array
+	If false, the method returns an object
+	* see output options above 
+. min (integer)
+	Number of reviews required to be listed
+
+### Ranking.bayesian(ratings, returnArray);
+
+Parameters:
+. ratings (Array)
+	Array with data 
+	* see input options above
+. returnArray (boolean)
+	If true, the method returns an array
+	If false, the method returns an object
+	* see output options above
+
--- smart-ranking.js
+++ smart-ranking.js
@@ -1,48 +1,73 @@
 (function() {
-    
-    /*
-        var ratings = [
-            { votes: [3,2,1], id:'Batman' },
-            { votes: [1,2,3,4,5], id:'Robin' }
-        ];
-    */
 
     var Ranking = (function() {
         
-        var _getStats = function(ratings){
+        var _getStats = function(ratings, returnArray){
             
             var sumTotalVotes = 0;
             var numTotalVotes = 0;
 
             var numRatings = ratings.length;
             
-            var scores = {};
+            var scores = returnArray ? [] : {};
             
             for(var i=0;i<numRatings;i++) {
                 
                 var review = ratings[i];
                 var votes = review.votes;
-                var numVotes = votes.length;
-                var sumVotes = 0;
+                if(votes) {
 
-                for(var j=0;j<numVotes;j++) {
-                    sumVotes+=votes[j];
-                }
-                
-                sumTotalVotes+=sumVotes;
-                numTotalVotes+=numVotes;
-                
-                scores[review.id] = {
-                    _id: review.id,
-                    _sumVotes: sumVotes,
-                    _numVotes: numVotes,
-                    _avgRating: (sumVotes/numVotes)
+                    var numVotes = votes.length;
+                    var sumVotes = 0;
+
+					var id = review.id ? review.id : review._id.id;
+
+                    for(var j=0;j<numVotes;j++) {
+                        sumVotes+=votes[j];
+                    }
+
+                    sumTotalVotes+=sumVotes;
+                    numTotalVotes+=numVotes;
+                    
+                    var obj = {
+                        id: review._id,
+                        sumVotes: sumVotes,
+                        numVotes: numVotes,
+                        avgRating: (sumVotes/numVotes)
+                    }
+                    
+                    if(returnArray) {
+                        scores.push(obj);
+                    } else {
+                        scores[id] = obj;
+                    }
+                    
+                } else {
+                    
+                    sumTotalVotes+=review.sumVotes;
+                    numTotalVotes+=review.numVotes;
+                    
+					var id = review.id ? review.id : review._id.id;
+
+                    var obj = {
+                        id: id,
+                        sumVotes: review.sumVotes,
+                        numVotes: review.numVotes,
+                        avgRating: (review.sumVotes/review.numVotes)
+                    }
+                    
+                    if(returnArray) {
+                        scores.push(obj);
+                    } else {
+                        scores[obj.id] = obj;
+                    }
+                    
                 }
                 
             }
             
-            scores._avgRatings = sumTotalVotes/numTotalVotes;
-            scores._avgVotes = numTotalVotes/numRatings;
+            scores.avgRatings = sumTotalVotes/numTotalVotes;
+            scores.avgVotes = numTotalVotes/numRatings;
             
             return scores;
             
@@ -50,12 +75,12 @@
         
         // based on this article:
         // http://blog.linkibol.com/2010/05/07/how-to-build-a-popularity-algorithm-you-can-be-proud-of/
-        var bayesian = function(ratings){
-            
-            var scores = _getStats(ratings);
+        var bayesian = function(ratings, returnArray){
+	
+            var scores = _getStats(ratings, returnArray);
             for(var id in scores) {
                 // br = ( (avg_num_votes * avg_rating) + (this_num_votes * this_rating) ) / (avg_num_votes + this_num_votes)
-                scores[id].score = ( (scores._avgVotes * scores._avgRatings) + (scores[id]._numVotes * scores[id]._avgRating) ) / (scores._avgVotes + scores[id]._numVotes);
+                scores[id].score = ( (scores.avgVotes * scores.avgRatings) + (scores[id].numVotes * scores[id].avgRating) ) / (scores.avgVotes + scores[id].numVotes);
             }
 
             return scores;
@@ -64,11 +89,11 @@
         
         // based on this article:
         // http://leedumond.com/blog/the-wisdom-of-crowds-implementing-a-smart-ranking-algorithm
-        var amazon = function(ratings, min){
-            
+        var imdb = function(ratings, returnArray, min){
+
             if(!min) min = 3;
             
-            var scores = _getStats(ratings);
+            var scores = _getStats(ratings, returnArray);
             for(var id in scores) {
                 /*
                 (W) = (v ÷ (v+m)) × R + (m ÷ (v+m)) × C
@@ -78,10 +103,10 @@
                 m = minimum reviews required to be listed (currently 10)
                 C = the mean across the list (currently 2.5)
                 */
-                var v = scores[id]._numVotes;
-                var R = scores[id]._avgRating;
+                var v = scores[id].numVotes;
+                var R = scores[id].avgRating;
                 var m = min;
-                var C = scores._avgRatings;
+                var C = scores.avgRatings;
                 scores[id].score = (v / (v+m)) * R + (m / (v+m)) * C;
             }
             
@@ -91,7 +116,7 @@
         
         return {
             bayesian: bayesian,
-            amazon: amazon
+            imdb: imdb
         };
         
     })();
